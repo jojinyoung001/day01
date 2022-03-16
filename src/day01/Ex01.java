3c7a0559bc323fbbeb36bd770074075326241630
jo <jjy>
2022-03-16 11:36:14 +0900

    Ex01 print

---
+++ src/day01/Ex01.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class Ex01 {
+	public static void main(String[] arge) {
+		//출력하는 기능
+		/*
+		  강아지가 아파
+		  왜 아파
+		  그냥 아파
+		 */
+		
+		System.out.println("안녕하세요!!!");
+		System.out.println("자바시간입니다!!!");
+		
+		System.out.print("출력");
+		System.out.print("입니다.");
+		
+	}
+
+}

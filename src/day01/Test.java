3da77ac37155b368491695b3a497489eaa0cf9c3
jo <jjy>
2022-03-16 14:15:26 +0900

    오늘 심심

---
+++ src/day01/Test.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("============================================");
+		System.out.println("이름\t나이\t전화번호\t회비");
+		System.out.println("============================================");
+		System.out.println("홍길동\t"+"\""+15+"\"\t"+3672+"-"+1234+"\t"+"\\"+200000);
+		System.out.println("고길동\t"+"\""+15+"\"\t"+2238+"-"+1234+"\t"+"\\"+300000);
+		System.out.println("김말이\t"+"\""+15+"\"\t"+1234+"-"+1234+"\t"+"\\"+500000);
+		System.out.println("--------------------------------------------");
+		System.out.println("총합계\t\t\t\t"+"\\"+1000000);
+		System.out.println("--------------------------------------------");
+
+	}
+
+}
